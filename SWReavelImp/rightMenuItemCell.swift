adb05a79125c767202a70389f9858fb3cd30eb66
Stephan <vanaf1979@gmail.com>
2017-06-22 21:07:30 +0200

    no message

--- SWReavelImp/rightMenuItemCell.swift
+++ SWReavelImp/rightMenuItemCell.swift
@@ -34,7 +34,7 @@ class rightMenuItemCell: baseCell
     
     func setupNameLabel()
     {
-        nameLabel.leftAnchor.constraint( equalTo: self.contentView.leftAnchor, constant: +25 ).isActive = true
+        nameLabel.rightAnchor.constraint( equalTo: self.contentView.rightAnchor, constant: -25 ).isActive = true
         nameLabel.topAnchor.constraint( equalTo: self.contentView.topAnchor, constant: +10 ).isActive = true
         nameLabel.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, constant: -10).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
